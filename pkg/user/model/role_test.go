5d644f06664ef7ce83f032ee06041b1be9352cd7
Matheus Alves <malvgomes@gmail.com>
2022-10-09 15:37:46 -0300

    chore: implement user model test

---
+++ pkg/user/model/role_test.go
@@ -0,0 +1,27 @@
+package model_test
+
+import (
+	"maintenance-task/pkg/user/model"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRole_IsValid(t *testing.T) {
+	tests := []struct {
+		name  string
+		role  model.Role
+		valid bool
+	}{
+		{"manager", model.Manager, true},
+		{"technician", model.Technician, true},
+		{"invalid", "Invalid", false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.valid, test.role.IsValid())
+		})
+	}
+
+}
---
+++ pkg/user/model/user_test.go
@@ -0,0 +1,94 @@
+package model_test
+
+import (
+	"maintenance-task/pkg/user/model"
+	"maintenance-task/shared/pointer"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateUser_IsValid(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *model.CreateUser
+		valid bool
+	}{
+		{
+			"Valid - All values", &model.CreateUser{
+				Username:  "username",
+				Password:  "pass",
+				FirstName: "firstName",
+				LastName:  pointer.String("lastName"),
+				UserRole:  model.Technician,
+			},
+			true,
+		},
+		{
+			"Valid - required values", &model.CreateUser{
+				Username:  "username",
+				Password:  "pass",
+				FirstName: "firstName",
+				UserRole:  model.Technician,
+			},
+			true,
+		},
+		{
+			"Invalid - received nil",
+			nil,
+			false,
+		},
+		{
+			"Invalid - received empty",
+			&model.CreateUser{},
+			false,
+		},
+		{
+			"Invalid - received invalid role",
+			&model.CreateUser{
+				Username:  "username",
+				Password:  "pass",
+				FirstName: "firstName",
+				UserRole:  "invalid",
+			},
+			false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.valid, test.input.IsValid())
+		})
+	}
+}
+
+func TestDeleteUser_IsValid(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *model.DeleteUser
+		valid bool
+	}{
+		{
+			"Valid", &model.DeleteUser{
+				Username: "username",
+			},
+			true,
+		},
+		{
+			"Invalid - received nil",
+			nil,
+			false,
+		},
+		{
+			"Invalid - received empty",
+			&model.DeleteUser{},
+			false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.valid, test.input.IsValid())
+		})
+	}
+}
